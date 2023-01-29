55449fa17726c8aa479ea1dd14b8f1b068f89079
nadeemabbas0474 <nadeemabbas0474@gmail.com>
2023-01-29 11:55:24 +0500

    theme configure

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,10 +1,12 @@
 import { MediumProvider } from "@/context/MediumContexxt";
 import "../styles/globals.css";
-
+import { ThemeProvider } from 'next-themes';
 export default function App({ Component, pageProps }) {
   return (
+    <ThemeProvider>
     <MediumProvider>
       <Component {...pageProps} />
     </MediumProvider>
+    </ThemeProvider>
   );
 }
